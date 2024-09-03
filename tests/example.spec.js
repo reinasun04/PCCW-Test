8b2918c45078ec645a819e0b9b371083680e69a0
Reina Sun <reina@wizy.io>
2024-09-03 13:25:07 +0800

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://example.com');
+  const title = await page.title();
+  console.log(title);
+  expect(title).toBe('Example Domain');
+});
