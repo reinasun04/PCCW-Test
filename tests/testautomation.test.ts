2bbd278c24192235909a491fdc9dc9d5719a3a06
reinasun04 <reina@wizy.io>
2024-09-03 13:49:40 +0800

    Update testautomation.test.ts

--- tests/testautomation.test.ts
+++ tests/testautomation.test.ts
@@ -3,10 +3,8 @@ import { test, expect } from '@playwright/test';
 test.describe('Valid credentials', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://practicetestautomation.com/practice-test-login/');
-    await page.waitForSelector('#username'); // Wait up to 5 seconds for the text to be visible
+    await page.waitForSelector('#username'); 
     await page.locator('#username').fill('student');
-    //await page.fill('#username', "student");
-    //await page.fill('#password', "Password123");
     await page.locator('#password').fill('Password123');
     await page.click('button[id="submit"]');
   });
@@ -14,6 +12,6 @@ test.describe('Valid credentials', () => {
   test('1_loginViaLocal_validLogin', async ({ page }) => {
     await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
     const textElement = page.locator('text=Logged In Successfully');
-    await expect(textElement).toBeVisible({ timeout: 5000 }); // Wait up to 5 seconds for the text to be visible
+    await expect(textElement).toBeVisible({ timeout: 5000 }); 
     });
 });
